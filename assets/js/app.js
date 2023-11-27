d706f90930b90035cee7edfd240dbc5835f3cdcf
Oleg19023 <96693781+Oleg19023@users.noreply.github.com>
2023-11-27 20:54:57 +0200

    update js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,16 +1,15 @@
-
 // Задача №1
-const temperatures = [-48, -46, 48, 27, -20, -35, 43, 4, 9, 10, 41, -46, -4, 0, -38, -49, 25, -46, -48, -23, -25, -22, 12, 38, 19, -20, 26, 4, 19, 23, 26, -41, 4, -13, -9, -11, -7, 38, 27, 41, 14, -35, -38, -44, -44, -22, -24, 29, -32, 41, 7, -25, 3, 27, -45, 10, 48, 8, -34, -49, 17, -16, 41, -11, -50, -6, -34, 20, 14, -18, 39, -28, -33, -27, -48, 40, -37, -44, 0, 46, 36, -34, -50, 8, -3, 26, 40, 10, -36, 24];
-
 // Подсчет заморозков 
 function countFreezes(temperatures) {
     let freezeCount = 0;
     let longestFreeze = 0;
     let currentFreeze = 0;
 
-    for (let i = 0; i <= temperatures.length; i++) {
-        if (i < temperatures.length && temperatures[i] < 0) {
+    for (let i = 0; i < temperatures.length - 1; i++) {
+        if (temperatures[i] >= 0 && temperatures[i + 1] < 0) {
             freezeCount++;
+            currentFreeze++;
+        } else if (temperatures[i] < 0) {
             currentFreeze++;
         } else {
             if (currentFreeze > longestFreeze) {
@@ -20,8 +19,15 @@ function countFreezes(temperatures) {
         }
     }
 
+    // Дополнительная проверка для последнего дня
+    if (temperatures[temperatures.length - 1] < 0 && currentFreeze > longestFreeze) {
+        longestFreeze = currentFreeze;
+    }
+
     return { freezeCount, longestFreeze };
 }
+
+const temperatures = [-48, -46, 48, 27, -20, -35, 43, 4, 9, 10, 41, -46, -4, 0, -38, -49, 25, -46, -48, -23, -25, -22, 12, 38, 19, -20, 26, 4, 19, 23, 26, -41, 4, -13, -9, -11, -7, 38, 27, 41, 14, -35, -38, -44, -44, -22, -24, 29, -32, 41, 7, -25, 3, 27, -45, 10, 48, 8, -34, -49, 17, -16, 41, -11, -50, -6, -34, 20, 14, -18, 39, -28, -33, -27, -48, 40, -37, -44, 0, 46, 36, -34, -50, 8, -3, 26, 40, 10, -36, 24];
 
 const { freezeCount, longestFreeze } = countFreezes(temperatures);
 
@@ -32,6 +38,8 @@ console.log(`Количество заморозков: ${freezeCount}`);
 console.log(`Самый длинный период заморозков: ${longestFreeze} дней`);
 console.log(`............................................`);
 console.log(`Задача №2`);
+
+
 
 
 
